9a2fdf9bbf46f261a6a8e6e54c715aa5f6d465c6
Luís Pinto 1180965 <1180965@isep.ipp.pt>
2020-06-16 19:20:40 +0100

    feat(ChooseFileView): File picker is now responsible for the initial validation

--- src/views/ChooseFileView.js
+++ src/views/ChooseFileView.js
@@ -3,28 +3,29 @@ import Button from '../components/Button'
 import PropTypes from 'prop-types'
 import '../styles/views/Views.css'
 
+/**
+ * This view is responsible for the input of the PDF file. After a correct PDF 
+ * file is inserted, it is sent to the parent.
+ *
+ * THIS VIEW ISN'T RESPONSIBLE FOR THE CONVERSION, BUT LoadPDFView IS
+ *
+ * @param {func} nextView - Prop that indicates to the parent that the new view 
+ * can be displayed, it also receives the selected file object
+ */
 const ChooseFileView = ({ nextView }) => {
-  // Stores the text to display
-  const [warningText, setWarningText] = useState('Select the PDF you want to change:')
-
   /**
    * Creates a file picker
-   * If the user picks a valid file it will move to the next view
-   * If not, it will display a warning
+   * When the user picks a file, it will move to the next view with this file
    */
   const filePicker = () => {
     var input = document.createElement('input')
     input.type = 'file'
+    input.accept = 'application/pdf'
 
     input.onchange = e => {
-      console.log(e.target.files[0])
       const file = e.target.files[0]
-      // Check if the file is valid
-      if (file.name.endsWith('.pdf')) {
-        nextView(file)
-      } else {
-        setWarningText('Uhm, the file you selected isn\'t a PDF 😕. Try another one.')
-      }
+      input.remove()
+      nextView(file)
     }
 
     input.click()
@@ -32,7 +33,7 @@ const ChooseFileView = ({ nextView }) => {
 
   return (
     <div className='centered-view'>
-      <p className='view-text'>{warningText}</p>
+      <p className='view-text'>Select the PDF you want to change:</p>
       <Button text='Choose File' style='success' onClick={filePicker} />
     </div>
   )

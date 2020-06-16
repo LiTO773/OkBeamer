ef43598fd4c3ad30fea4e34d25488e912c8b13df
Luís Pinto 1180965 <1180965@isep.ipp.pt>
2020-06-16 20:12:35 +0100

    feat(Spinner): Created a Loading Spinner
    Major props to: https://projects.lukehaas.me/css-loaders/

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/components/Spinner.css'
+
+// Spinner animation credit: https://github.com/lukehaas/css-loaders
+
+export default () => {
+  return (
+    <div className='loader' />
+  )
+}

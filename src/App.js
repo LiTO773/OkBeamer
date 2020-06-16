aec7cb5687ac95ea151c7608e3cf7e2974182b1e
Luís Pinto 1180965 <1180965@isep.ipp.pt>
2020-06-16 20:14:01 +0100

    feat(pdfjs): pdfjs-dist added and configured

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Header from './Header.js'
 import Views from './Views.js'
 import './styles/App.css'
 
+// Setup PDFJS worker
+// Source: https://github.com/wojtekmaj/react-pdf#standard-browserify-and-others
+import pdfjs from 'pdfjs-dist'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
 function App () {
   return (
     <div className='app'>

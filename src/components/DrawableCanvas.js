8bebbf2318590982103329d1daeff2ff1dca5fdb
Luís Pinto 1180965 <1180965@isep.ipp.pt>
2020-06-21 20:16:04 +0100

    feat(DrawableCanvas): Rectangle is now drawn in real time
    The canvas is now also properly rerendered
    Added refs
    TODO: Properly scale the canvas

--- src/components/DrawableCanvas.js
+++ src/components/DrawableCanvas.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import '../styles/components/DrawableCanvas.css'
+
+// TODO correctly scale the canvas
 
 /**
  * The DrawableCanvas component is responsible for rendering an interactive
@@ -15,59 +17,136 @@ import '../styles/components/DrawableCanvas.css'
  * @param {object} props Canvas props (listed above)
  */
 const DrawableCanvas = ({ pdf, setSelection }) => {
+  // #region States and refs
+  // Avoid rerenders of the canvas
+  const [rendered, setRendered] = useState(false)
+
+  // The variables below are used to render the page to the canvas
+  const canvas = useRef(null)
+  const renderedPage = useRef(null)
+  const renderedContext = useRef(null)
+
   // Coordinates state
-  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 })
-
-  // After the component is mounted, render the canvas
-  useEffect(() => {
-    pdf.getPage(1).then(page => {
-      const scale = 1
-      const viewport = page.getViewport({ scale })
-
-      const canvas = document.getElementById('drawable-canvas')
-      const context = canvas.getContext('2d')
-
-      const renderContext = {
-        canvasContext: context,
-        viewport
-      }
-      page.render(renderContext)
-    })
+  const [coordinates, setCoordinates] = useState({
+    startX: -1,
+    startY: -1,
+    endX: -1,
+    endY: -1
   })
 
-  // Handle mouse down (start)
-  const handleMouseDown = ({ nativeEvent }) => {
-    // console.log(e.screenX)
-    // console.log(e.screenY)
-    setCoordinates({
-      x: nativeEvent.x,
-      y: nativeEvent.y
-    })
+  // Stores if the mouse is being pressed
+  const mouseIsDown = useRef(false)
+  // #endregion States and refs
+
+  /**
+   * After the component is mounted, render the canvas
+   * This canvas render will only happen once, with the help of the rendered
+   * state
+   */
+  useEffect(() => {
+    // Won't rerender the canvas if it was already rendered
+    if (!rendered) {
+      pdf.getPage(1).then(page => {
+        // Render the first page in the PDF
+        const scale = 1
+        const viewport = page.getViewport({ scale })
+
+        const context = canvas.current.getContext('2d')
+        context.fillStyle = 'rgba(0, 102, 204)'
+
+        const renderContext = {
+          canvasContext: context,
+          viewport
+        }
+        page.render(renderContext)
+
+        // Persist the required variables for rerenders
+        renderedPage.current = page
+        renderedContext.current = renderContext
+        setRendered(true)
+      })
+    }
+  })
+
+  /**
+   * Rerenders the page in the canvas
+   */
+  const rerenderPage = () => {
+    renderedPage.current.render(renderedContext.current)
   }
 
-  // Handle mouse up (finish)
+  /**
+   * Handles the mouse down event. This means that the user wants to draw a
+   * rectangle.
+   *
+   * @param {Object} param0 Target, in this case it's used to get the mouse
+   * coordinates
+   */
+  const handleMouseDown = ({ nativeEvent }) => {
+    // Remove the previous rectangle by rerendering the page
+    rerenderPage()
+
+    // Start the rectangle drawing process
+    mouseIsDown.current = true
+    setCoordinates(coords => ({
+      startX: nativeEvent.x,
+      startY: nativeEvent.y,
+      endX: -1,
+      endY: -1
+    }))
+  }
+
+  /**
+   * Handles the mouse up event. This means that the user has finished drawing a
+   * rectangle.
+   *
+   * @param {Object} param0 Target, in this case it's used to get the mouse
+   * coordinates
+   */
   const handleMouseUp = ({ nativeEvent }) => {
-    const canvas = document.getElementById('drawable-canvas')
-    const rect = canvas.getBoundingClientRect()
-    const context = canvas.getContext('2d')
-    context.beginPath()
-    context.fillStyle = 'rgba(0, 102, 204,0.5)'
-    // Draw the rectangle
-    context.fillRect(
-      coordinates.x - rect.left,
-      coordinates.y - rect.top,
-      nativeEvent.x - coordinates.x,
-      nativeEvent.y - coordinates.y
-    )
+    // Stop drawing
+    mouseIsDown.current = false
+
+    // Save the end coordinates
+    setCoordinates(coords => ({
+      ...coords,
+      endX: nativeEvent.x,
+      endY: nativeEvent.y
+    }))
+  }
+
+  /**
+   * Draws the rectangle while the user moves it's cursor. It's a bit janky but
+   * it allows a rectangle to be drawn.
+   *
+   * @param {Object} param0 Target, in this case it's used to get the mouse
+   * coordinates
+   */
+  const handleMovement = ({ nativeEvent }) => {
+    if (canvas.current != null && mouseIsDown.current) {
+      const rect = canvas.current.getBoundingClientRect()
+      const context = canvas.current.getContext('2d')
+
+      // Draw the rectangle
+      context.beginPath()
+      context.fillRect(
+        coordinates.startX - rect.left,
+        coordinates.startY - rect.top,
+        nativeEvent.x - coordinates.startX,
+        nativeEvent.y - coordinates.startY
+      )
+    }
   }
 
   return (
     <canvas
+      ref={canvas}
       id='drawable-canvas'
       height='540'
       width='720'
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
+      onMouseMove={handleMovement}
     />
   )
 }

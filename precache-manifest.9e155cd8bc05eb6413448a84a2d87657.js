cfabb79ac1b0e62c4e4d3f1329029f1b98b50a25
Luís Pinto 1180965 <1180965@isep.ipp.pt>
2020-06-24 19:36:51 +0100

    Updates

---
+++ precache-manifest.9e155cd8bc05eb6413448a84a2d87657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da84bf78a62a9bc11488c92d3568eb3c",
+    "url": "/OkBeamer/index.html"
+  },
+  {
+    "revision": "6c6a7f5bb748bb7bd3c9",
+    "url": "/OkBeamer/static/css/main.3df02398.chunk.css"
+  },
+  {
+    "revision": "0409c35b103522305322",
+    "url": "/OkBeamer/static/js/2.55e29226.chunk.js"
+  },
+  {
+    "revision": "7c74ae27a4fdccfefc718704e320826e",
+    "url": "/OkBeamer/static/js/2.55e29226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6a7f5bb748bb7bd3c9",
+    "url": "/OkBeamer/static/js/main.2f8dbe91.chunk.js"
+  },
+  {
+    "revision": "676d73371443ce3f958a",
+    "url": "/OkBeamer/static/js/runtime-main.d2221735.js"
+  }
+]);
